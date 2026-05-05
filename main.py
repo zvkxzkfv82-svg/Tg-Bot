e46535382a81a8bc99f29d3e425287610f603d27
zvkxzkfv82-svg <zvkxzkfv82@privaterelay.appleid.com>
2026-05-05 17:42:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -55,9 +55,10 @@ def send_email(subject, zip_path):
             filename=os.path.basename(zip_path),
         )
 
-    with smtplib.SMTP_SSL("smtp.yandex.ru", 465) as smtp:
-        smtp.login(EMAIL_USER, EMAIL_PASS)
-        smtp.send_message(msg)
+    with smtplib.SMTP("smtp.yandex.ru", 587) as smtp:
+    smtp.starttls()
+    smtp.login(EMAIL_USER, EMAIL_PASS)
+    smtp.send_message(msg)
 
 # =========================
 # ZIP + SEND
