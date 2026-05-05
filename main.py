96532121ff171cb89e23ef5153f8e6bd6b0058d2
zvkxzkfv82-svg <zvkxzkfv82@privaterelay.appleid.com>
2026-05-05 14:07:53 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет! Бот работает 🚀")
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
